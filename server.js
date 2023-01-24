38b8d6f13cfdafac468abe452f3a547c018656a0
bklingelfus <bernardoklingelfus@gmail.com>
2023-01-23 23:37:11 -0300

    procfile

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/yahoo-finance/:module/:symbol', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on port 3000');
 });
