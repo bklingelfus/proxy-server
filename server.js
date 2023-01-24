9a065ecd4f16fc9421218c721412d8658e7c7737
bklingelfus <bernardoklingelfus@gmail.com>
2023-01-23 23:44:13 -0300

    procfile

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const https = require('https');
 const cors = require('cors');
 const app = express();
 
-const allowedOrigins = ['https://finance-app-fe.herokuapp.com/'];
+// const allowedOrigins = ['https://finance-app-fe.herokuapp.com/'];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // allow requests with no origin 
+//         // (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 app.get('/yahoo-finance/:module/:symbol', (req, res) => {
     https.get('https://query1.finance.yahoo.com/v10/finance/quoteSummary/'+[req.params.symbol]+'?modules='+[req.params.module], { headers: { 'Accept': 'application/json' } }, (apiRes) => {
